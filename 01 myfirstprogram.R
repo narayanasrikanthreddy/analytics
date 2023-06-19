1eb576630be6f22dce4e36937eff5c900d2f0d76
narayanasrikanthreddy <49014360+narayanasrikanthreddy@users.noreply.github.com>
2023-06-19 15:31:37 +0530

    Add files via upload

---
+++ 01 myfirstprogram.R
@@ -0,0 +1,26 @@
+#This is comment using # symbol
+#Use ctrl+enter to run the current line or selection
+
+#Select the next line and press ctrl+enter
+5+3
+
+#Put the cursor at the end of the line and press ctrl+enter
+6-7
+
+#Put the cursor at the end of the line and press ctrl+enter
+
+11*56
+
+
+#Select both the code lines and press ctrl+enter
+75/25
+60/30
+
+#Select both the lines given below and press ctrl+enter
+5+5
+#5+5
+
+
+#Now press ctrl+shift+enter to run all code 
+#Note that you need not select anything or look at the position of cursor
+
